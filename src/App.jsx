6aaac903c477457a60b8bb33fec71654a20ab6fe
Chanithi Perera <chanithidehansa@gmail.com>
2026-06-28 11:49:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -444,7 +444,7 @@ function App() {
             </button>
             <div className="nav-links">
               <a href="#about">About Me</a>
-              <a href="#projects">Portfolio</a>
+              <a href="#projects">Projects</a>
               <a href="#skills">Skills</a>
               <a href="#contact">Contact</a>
             </div>
